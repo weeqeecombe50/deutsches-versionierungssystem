2a6577b336b1ce2d72676247d24e587e9b68ddda
weeqeecombe50 <mynieagbona@hotmail.com>
2025-09-07 22:35:21 +0300

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+class VersionierungSystem {
+    constructor(projektname) {
+        this.projektname = projektname;
+        this.versions = [];
+    }
+
+    init() {
+        const projectPath = path.join(__dirname, this.projektname);
+        fs.mkdirSync(projectPath);
+        fs.writeFileSync(path.join(projectPath, 'README.md'), '# ' + this.projektname + '\n\nDieses Verzeichnis enthält alle Versionen des Projekts.');
+        console.log(`Projekt ${this.projektname} initialisiert.`);
+    }
+
+    addVersion(version) {
+        this.versions.push(version);
+    }
+
+    listVersions() {
+        console.log('Versionshistorie:');
+        this.versions.forEach((v) => {
+            console.log(v);
+        });
+    }
+}
+
+module.exports = VersionierungSystem;
